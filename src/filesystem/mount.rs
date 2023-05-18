534f825b186b2d558ce697d4e8111f69506d54fb
Benj Fassbind <randombenj@gmail.com>
2023-05-18 17:38:36 +0200

    Remove unnecessary mounts

--- src/filesystem/mount.rs
+++ src/filesystem/mount.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
-use nix::mount::{MsFlags, mount};
+use log::debug;
+use nix::mount::{mount, MsFlags};
 
 /// Mounts the proc filesystem to '/proc'.
 ///
@@ -15,14 +16,12 @@ pub fn mount_proc() {
         MsFlags::MS_RDONLY,
         None::<&str>,
     )
-    .unwrap_or_else(|err| panic!("[panic]: mounting /proc failed!!\n{:?}", err));
+    .unwrap_or_else(|err| panic!("Mounting /proc failed!!\n{:?}", err));
 }
 
 /// Initializes the filesystem:
 ///
-/// - the root disk is mounted to '/'
-/// - devtmpfs is mounted to '/dev'
-/// - sysfs is mounted to '/sys'
+/// - Mount the root disk is mounted to '/'
 ///
 /// # Arguments
 ///
@@ -33,6 +32,7 @@ pub fn mount_proc() {
 ///
 /// Panics if mounting any of the filesystems fails.
 pub fn mount_filesystem(root_disk: &str) {
+    debug!("Mounting '/ -> {}'", root_disk);
     mount(
         Some(root_disk),
         Path::new("/"),
@@ -40,23 +40,5 @@ pub fn mount_filesystem(root_disk: &str) {
         MsFlags::MS_REMOUNT,
         None::<&str>,
     )
-    .unwrap_or_else(|err| panic!("[panic]: mounting '/ -> {}' failed!!\n{:?}", root_disk, err));
-
-    mount(
-        None::<&str>,
-        Path::new("/sys"),
-        Some("sysfs"),
-        MsFlags::MS_RDONLY,
-        None::<&str>,
-    )
-    .unwrap_or_else(|err| panic!("[panic]: mounting /sys failed!\n{:?}", err));
-
-    mount(
-        None::<&str>,
-        Path::new("/dev"),
-        Some("devtmpfs"),
-        MsFlags::MS_RDONLY,
-        None::<&str>,
-    )
-    .unwrap_or_else(|err| panic!("[panic]: mounting /dev failed!\n{:?}", err));
+    .unwrap_or_else(|err| panic!("Mounting '/ -> {}' failed!!\n{:?}", root_disk, err));
 }
